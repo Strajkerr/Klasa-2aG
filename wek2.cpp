0c054935f67f19ff2631da1b9b7288738df4b3e8
Strajkerr <71469217+Strajkerr@users.noreply.github.com>
2020-10-05 16:11:12 +0200

    Create wek2.cpp

---
+++ wek2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    double wek[100][2], poz[100];
+    for(int i = 0; i < 100; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            cin >> wek[i][j];
+        }
+            poz[i] = sqrt(pow(wek[i][0], 2)+(pow(wek[i][1], 2)));
+    }
+    for(int h = 0; h < 100; h++)
+    {
+        printf("%.2f", abs(poz[h]));
+        cout << endl;
+    }
+}
